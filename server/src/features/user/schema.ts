7de8cc2f0a69141b080c0ff4490b4ee30aa13844
sompakorn-lap <sompakorn.lap@student.mahidol.edu>
2025-01-11 16:50:55 +0700

    feat(server): integreate drizzle with typebox

--- server/src/features/user/schema.ts
+++ server/src/features/user/schema.ts
@@ -1,8 +1,12 @@
-import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+import { integer, pgTable, uuid, varchar } from "drizzle-orm/pg-core";
+import { createSelectSchema } from "drizzle-typebox";
 
-export const usersTable = pgTable("users", {
-  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+export const users = pgTable("users", {
+  userId: uuid().defaultRandom().primaryKey(),
   name: varchar({ length: 255 }).notNull(),
   age: integer().notNull(),
   email: varchar({ length: 255 }).notNull().unique(),
 });
+
+export const UserSchema = createSelectSchema(users);
+export type UserType = typeof UserSchema.static;
